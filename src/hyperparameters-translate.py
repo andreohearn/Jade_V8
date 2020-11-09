abd300d5eec3a4583f068cd66e37a9b5b6566a86
Jess Edmund Fan <jessefan2002@gmail.com>
2020-11-08 18:23:27 -0800

    Update hyperparameters-translate.py

--- src/hyperparameters-translate.py
+++ src/hyperparameters-translate.py
@@ -7,9 +7,26 @@ import trax.supervised.trainer_lib
 # Parameters that will vary between experiments:
 # ==============================================================================
 train.model = @trax.models.ReformerLM
+# Our model will have 6 layers, alternating between the LSH attention proposed
+# in the Reformer paper and local attention within a certain context window.
+n_layers = 6
+attn_type = [
+  @trax.layers.SelfAttention,
+  @LSHSelfAttention,  
+  @trax.layers.SelfAttention,
+  @LSHSelfAttention,
+  @trax.layers.SelfAttention,
+  @LSHSelfAttention,
+  ]
+share_qk = False  # LSH attention ignores this flag and always shares q & k
+n_heads = 6
+attn_kv = 64
+dropout = 0.1
+n_tokens = 512
+
 # Parameters for multifactor:
 # ==============================================================================
-multifactor.constant = 0.01
+multifactor.constant = 0.088
 multifactor.decay_factor = 0.5
 multifactor.factors = 'constant * linear_warmup * rsqrt_decay'
 multifactor.steps_per_cycle = 100000
@@ -18,19 +35,43 @@ multifactor.warmup_steps = 8000
 
 # Parameters for Adam:
 # ==============================================================================
-Adam.b1 = 0.9
-Adam.b2 = 0.98
-Adam.eps = 1e-09
-Adam.weight_decay_rate = 1e-05
+Adam.weight_decay_rate=1e-05
+Adam.b1 = 0.86
+Adam.b2 = 0.92
+Adam.eps = 1e-9
+
+# Parameters for SelfAttention:
+# ==============================================================================
+trax.layers.SelfAttention.attention_dropout = 0.1
+trax.layers.SelfAttention.chunk_len = 64
+trax.layers.SelfAttention.n_chunks_before = 1
+trax.layers.SelfAttention.n_parallel_heads = 1
+
+# Parameters for LSHSelfAttention:
+# ==============================================================================
+LSHSelfAttention.attention_dropout = 0.0
+LSHSelfAttention.chunk_len = 64
+LSHSelfAttention.n_buckets = [64, 128]
+LSHSelfAttention.n_chunks_after = 0
+LSHSelfAttention.n_chunks_before = 1
+LSHSelfAttention.n_hashes = 2
+LSHSelfAttention.n_parallel_heads = 1
+LSHSelfAttention.predict_drop_len = 128
+LSHSelfAttention.predict_mem_len = 1024
 
 # Parameters for ReformerLM:
 # ==============================================================================
+ReformerLM.attention_type = %attn_type
+ReformerLM.d_attention_key = %attn_kv
+ReformerLM.d_attention_value = %attn_kv
 ReformerLM.d_model = 512
-ReformerLM.d_ff = 2048
-ReformerLM.dropout = 0.1
+ReformerLM.d_ff = 1024
+ReformerLM.dropout = %dropout
 ReformerLM.ff_activation = @trax.layers.Relu
-ReformerLM.max_len = 512
+ReformerLM.max_len = %n_tokens
 ReformerLM.mode = 'train'
-ReformerLM.n_layers = 6
-ReformerLM.n_heads = 4
+ReformerLM.n_heads = %n_heads
+ReformerLM.n_layers = %n_layers
 ReformerLM.vocab_size = 200
+ReformerLM.axial_pos_shape = (16,32)
+ReformerLM.d_axial_pos_embs= (128, 384)
