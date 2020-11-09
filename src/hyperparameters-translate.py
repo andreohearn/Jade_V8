64377ed86e2621d2a3ca102f8c9fd7b0cf388ec8
Jess Edmund Fan <jessefan2002@gmail.com>
2020-11-08 19:25:05 -0800

    Update hyperparameters-translate.py

--- src/hyperparameters-translate.py
+++ src/hyperparameters-translate.py
@@ -26,11 +26,9 @@ n_tokens = 256
 
 # Parameters for multifactor:
 # ==============================================================================
-multifactor.constant = 0.088
-multifactor.decay_factor = 0.5
+multifactor.constant = 0.01
 multifactor.factors = 'constant * linear_warmup * rsqrt_decay'
 multifactor.steps_per_cycle = 100000
-multifactor.steps_per_decay = 20000
 multifactor.warmup_steps = 8000
 
 # Parameters for Adam:
