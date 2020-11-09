47aac65cc47007c8bef7e3d8ea667e3f0f8f29bc
Jess Edmund Fan <jessefan2002@gmail.com>
2020-11-09 10:22:31 -0800

    more fixes

--- train.py
+++ train.py
@@ -74,21 +74,24 @@ print("These samples will not be included.")
 # What this does is take the big list of list question/answer tokenized arrays and concatanate the data split into fittable sizes
 # These are looped through, in order, to generate next outputs with context.
 
-def gen_inputs(n_devices):
+def gen_inputs(batch_size):
     while True:
         inputs = []
         mask = []
         #sometimes the pad amount is :((, so skip those; the data will be made up later
-        for i in range(n_devices):
-            PAD_AMOUNT=0
-            while PAD_AMOUNT <= 0:
-                current_sample = np.random.choice(len(IDS)-1, 1)[0]
-                SELECT=IDS[current_sample]
-                PAD_AMOUNT = (config_general["size"]) - len(SELECT)
+        PAD_AMOUNT=0
+        while PAD_AMOUNT <= 0:
+            current_sample = np.random.choice(len(IDS)-1, 1)[0]
+            SELECT=IDS[current_sample]
+            PAD_AMOUNT = (256) - len(SELECT)
+        for i in range(batch_size):
+            pad_amounts = np.random.choice(PAD_AMOUNT, 1)[0]
             IN,OUT=" ".join([str(value) for value in SELECT]).split(" 2 ")
-            SELECT=np.concatenate([np.asarray([2],dtype=np.int32), np.asarray(IN.split(" "), dtype=np.int32), np.asarray([2],dtype=np.int32) ,np.asarray(OUT.split(" "), dtype=np.int32), np.zeros(PAD_AMOUNT-1,dtype=np.int32)])
-            inputs.append(SELECT)
-            mask.append(np.concatenate([np.zeros(len(IN.split(" "))+2,dtype=np.int32), np.ones(len(OUT.split(" ")),dtype=np.int32), np.zeros(PAD_AMOUNT-1,dtype=np.int32)]))
+            INP=np.concatenate([np.zeros(pad_amounts,dtype=np.int32), np.asarray([2],dtype=np.int32), np.asarray(IN.split(" "), dtype=np.int32), np.asarray([2],dtype=np.int32) ,np.asarray(OUT.split(" "), dtype=np.int32), np.zeros(PAD_AMOUNT-pad_amounts,dtype=np.int32)])
+            #print(SELECT)
+            inputs.append(INP)
+            mask.append(np.concatenate([np.zeros(pad_amounts,dtype=np.int32), np.zeros(len(IN.split(" "))+2,dtype=np.int32), np.ones(len(OUT.split(" "))), np.zeros(PAD_AMOUNT-pad_amounts,dtype=np.int32)]))
+            #print(np.concatenate([np.zeros(len(IN.split(" "))+2,dtype=np.int32), np.ones(len(OUT.split(" "))), np.full(PAD_AMOUNT-1,2,dtype=np.int32)]))
         inputs = np.stack(inputs)
         mask = np.stack(mask)
         yield (inputs, inputs, mask)
