c80d099525c02f024b87bb6c99961bc8eee1d5b9
Jess Edmund Fan <jessefan2002@gmail.com>
2020-11-08 17:37:07 -0800

    Update train.py

--- train.py
+++ train.py
@@ -86,7 +86,7 @@ def gen_inputs(n_devices):
                 SELECT=IDS[current_sample]
                 PAD_AMOUNT = (config_general["size"]) - len(SELECT)
             IN,OUT=" ".join(SELECT).split(" 2 ")
-            SELECT=np.concatenate([np.asarray([2],dtype=np.int32), np.asarray(IN.split(" "), dtype=np.int32), np.asarray([2],dtype=np.int32) ,np.asarray(OUT.split(" "), dtype=np.int32), np.zeros(PAD_AMOUNT-1))], axis=1)
+            SELECT=np.concatenate([np.asarray([2],dtype=np.int32), np.asarray(IN.split(" "), dtype=np.int32), np.asarray([2],dtype=np.int32) ,np.asarray(OUT.split(" "), dtype=np.int32), np.zeros(PAD_AMOUNT-1)]), axis=1)
             inputs.append(SELECT)
             mask.append(np.concatenate([np.zeros(len(IN)+2), np.ones(len(OUT))], axis=1))
         inputs = np.stack(inputs)
